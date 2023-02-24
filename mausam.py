15cea224c7be61ab56ec7e69d9114bc7ef295274
Ayush Ranjan <88894139+ayushranjan856@users.noreply.github.com>
2023-02-24 07:52:10 +0530

    Update mausam.py

--- mausam.py
+++ mausam.py
@@ -1,23 +1,21 @@
-# import required modules
 from configparser import ConfigParser
 import requests
 from tkinter import *
 from tkinter import messagebox
 
-# extract key from the
-# configuration file
+# extract key from the configuration file
 config_file = "config.ini"
 config = ConfigParser()
 config.read(config_file)
 api_key = config['gfg']['api']
-url ="https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
-api_file="ayush"
-file_a=ConfigParser()
-file_a.read(api_file)
-api_key=file_A["api_key"]["key"]
+url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
 
-# explicit function to get
-# weather details
+api_file = "api.ini"
+file_a = ConfigParser()
+file_a.read(api_file)
+api_key = file_a["api_key"]["key"]
+
+# explicit function to get weather details
 def getweather(city):
     result = requests.get(url.format(city, api_key))
 
@@ -26,24 +24,22 @@ def getweather(city):
         city = json['name']
         country = json['sys']["country"]
         temp_kelvin = json['main']['temp']
-        temp_celsius = (temp_kelvin - 273.15)*9/5+32
+        temp_celsius = round((temp_kelvin - 273.15) * 9/5 + 32, 2)
         weather1 = json['weather'][0]['main']
-        result = [city, country, temp_kelvin,
-                 temp_celsius, weather1]
+        result = [city, country, temp_kelvin, temp_celsius, weather1]
         return result
     else:
         print("NO Content Found")
 
 
-# explicit function to
-# search city
+# explicit function to search city
 def search():
-    city = city.get()
+    city = city_text.get()
     weather = getweather(city)
     if weather:
         location_lbl['text'] = '{} ,{}'.format(weather[0], weather[1])
         temperature_label['text'] = str(weather[3]) + "   Degree Celsius"
-        weather1['text'] = weather[4]
+        weather_l['text'] = weather[4]
     else:
         messagebox.showerror('Error', "Cannot find {}".format(city))
 
@@ -51,6 +47,7 @@ def search():
 # Driver Code
 # create object
 app = Tk()
+
 # add title
 app.title("Weather App")
 app.config(background="black")
@@ -58,15 +55,19 @@ app.geometry("300x300")
 
 # add labels, buttons and text
 city_text = StringVar()
-city_entry = Entry(app, textvariable=city_text,fg="blue",font=("Arial",30,"bold"))
+city_entry = Entry(app, textvariable=city_text, fg="blue", font=("Arial", 30, "bold"))
 city_entry.pack()
-search_btn = Button(app, text="Search Weather",
-                    width=12, command=search)
-Search_btn.pack()
+
+search_btn = Button(app, text="Search Weather", width=12, command=search)
+search_btn.pack()
+
 location_lbl = Label(app, text="Location", font=('bold', 20))
 location_lbl.pack()
-temperature_label = Label(app, text="",font=("Arial",35,"bold"),bg="lightpink")
+
+temperature_label = Label(app, text="", font=("Arial", 35, "bold"), bg="lightpink")
 temperature_label.pack()
-weather_l = Label(app, text="",font=("Arial",35,"bold"),bg="lightgreen")
+
+weather_l = Label(app, text="", font=("Arial", 35, "bold"), bg="lightgreen")
 weather_l.pack()
+
 app.mainloop()
